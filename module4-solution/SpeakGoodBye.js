8873d6b0f8bc7625e7bea185ee6909de951bfba5
novgorodtsev <persinfo@yahoo.com>
2016-10-30 12:01:57 -0400

    module 4 solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -3,6 +3,7 @@
   var speakWord = "Good Bye";
   byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
+    document.write("<p>"+speakWord + " " + name+"</p>");
   }
   window.byeSpeaker = byeSpeaker;
 })(window);
--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -3,6 +3,7 @@
   var speakWord = "Hello";
   helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
+    document.write("<p>"+speakWord + " " + name+"</p>");
   }
   window.helloSpeaker = helloSpeaker;
 })(window);
